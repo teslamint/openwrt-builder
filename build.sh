d1c00c94be3c618457677b0d97913248bd515246
Jaehoon You <teslamint@gmail.com>
2019-09-14 15:14:11 +0900

    initial commit
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# clone openwrt and packages
+git clone https://github.com/openwrt/openwrt
+git clone -b openwrt-19.07 https://github.com/openwrt/luci
+git clone https://github.com/stangri/openwrt_packages stangri_openwrt_packages
+
+# prepare package
+cp -Rfv stangri_openwrt_packages/luci-app-vpn-policy-routing luci/applications/
+
+# copy some configs
+cp conf/config.iptime_a604m openwrt/.config
+cp conf/feeds.conf openwrt/
+
+pushd openwrt
+scripts/feeds update -a
+scripts/feeds install -a
+make defconfig
+make
+popd
