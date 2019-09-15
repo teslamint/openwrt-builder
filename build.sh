c203731c761cf1339a95c3e8d60638e1dfcd5846
Jaehoon You <teslamint@gmail.com>
2019-09-15 18:35:43 +0900

    update build.sh
    you can now run build.sh with options like -j7
    
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MAKEOPTS=$*
+MAKEOPTS=${MAKEOPTS:-"-j7"}
+
 # clone openwrt and packages
-git clone https://github.com/openwrt/openwrt
-git clone -b openwrt-19.07 https://github.com/openwrt/luci
-git clone https://github.com/stangri/openwrt_packages stangri_openwrt_packages
+git clone --depth=1 https://github.com/openwrt/openwrt
+git clone --depth=1 -b openwrt-19.07 https://github.com/openwrt/luci
+git clone --depth=1 https://github.com/stangri/openwrt_packages stangri_openwrt_packages
 
 # prepare package
 cp -Rfv stangri_openwrt_packages/luci-app-vpn-policy-routing luci/applications/
@@ -15,6 +18,6 @@ cp conf/feeds.conf openwrt/
 pushd openwrt
 scripts/feeds update -a
 scripts/feeds install -a
-make defconfig
-make
+make oldconfig
+make $MAKEOPTS
 popd
