3b6876b773e6445cdfb21fff05f8d781e878f85f
Jaehoon You <teslamint@gmail.com>
2019-09-22 14:47:49 +0900

    remove usage of src-link
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ MAKEOPTS=${MAKEOPTS:-"-j7"}
 
 # clone openwrt and packages
 git clone --depth=1 https://github.com/openwrt/openwrt
-git clone --depth=1 https://github.com/stangri/openwrt_packages stangri_openwrt_packages
 
 # copy some configs
 cp conf/config.iptime_a604m openwrt/.config
@@ -15,7 +14,8 @@ pushd openwrt
 
 # prepare package
 scripts/feeds update -a
-cp -Rfv ../stangri_openwrt_packages/luci-app-vpn-policy-routing feeds/luci/applications/
+cp -Rfv feeds/stangri/luci-app-vpn-policy-routing feeds/luci/applications/
+scripts/feeds update luci
 scripts/feeds install -a
 
 # build
