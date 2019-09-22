4c3d78adebc86715083d210da03fe99cf4e68203
Jaehoon You <teslamint@gmail.com>
2019-09-22 14:25:02 +0900

    fix: luci package is not shown on `scripts/feeds list`
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- build.sh
+++ build.sh
@@ -5,19 +5,20 @@ MAKEOPTS=${MAKEOPTS:-"-j7"}
 
 # clone openwrt and packages
 git clone --depth=1 https://github.com/openwrt/openwrt
-git clone --depth=1 -b openwrt-19.07 https://github.com/openwrt/luci
 git clone --depth=1 https://github.com/stangri/openwrt_packages stangri_openwrt_packages
-
-# prepare package
-cp -Rfv stangri_openwrt_packages/luci-app-vpn-policy-routing luci/applications/
 
 # copy some configs
 cp conf/config.iptime_a604m openwrt/.config
 cp conf/feeds.conf openwrt/
 
 pushd openwrt
+
+# prepare package
 scripts/feeds update -a
+cp -Rfv ../stangri_openwrt_packages/luci-app-vpn-policy-routing feeds/luci/applications/
 scripts/feeds install -a
+
+# build
 make oldconfig
 make $MAKEOPTS
 popd
