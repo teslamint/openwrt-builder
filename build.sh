62b3f565d0bd0389096c12a03f2905f3515ce5ea
Jaehoon You <teslamint@gmail.com>
2020-02-01 21:21:51 +0900

    update
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ MAKEOPTS=${MAKEOPTS:-"-j7"}
 
 # clone openwrt and packages
 git clone --depth=1 https://github.com/openwrt/openwrt
+git clone --depth=1 https://github.com/stangri/openwrt_packages
 
 # copy some configs
 cp conf/config.iptime_a604m openwrt/.config
@@ -14,7 +15,9 @@ pushd openwrt
 
 # prepare package
 scripts/feeds update -a
-cp -Rfv feeds/stangri/luci-app-vpn-policy-routing feeds/luci/applications/
+cp -Rfv ../openwrt_packages/vpn-policy-routing feeds/packages/net/
+cp -Rfv ../openwrt_packages/luci-app-vpn-policy-routing feeds/luci/applications/
+scripts/feeds update packages
 scripts/feeds update luci
 scripts/feeds install -a
 
