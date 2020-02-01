fc7aeeeffd49af1965da8ff7b6d2a36a98a06444
Jaehoon You <teslamint@gmail.com>
2020-02-02 03:06:35 +0900

    build image with toolchain
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- docker-build.A604M.sh
+++ docker-build.A604M.sh
@@ -2,5 +2,4 @@
 
 docker run --rm -it -v $PWD/bin:/home/build/openwrt/bin \
     -v $PWD/dl:/home/build/openwrt/dl \
-    -v $PWD/conf/config.iptime_a604m:/home/build/openwrt/.config \
-    openwrt-builder:latest
+    openwrt-builder:latest-a604m
