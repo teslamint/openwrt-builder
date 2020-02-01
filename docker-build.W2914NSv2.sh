e6b5cd03630b7c8fe6084f114439380bf2eb182e
Jaehoon You <teslamint@gmail.com>
2020-02-02 03:09:48 +0900

    fix build script
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- docker-build.W2914NSv2.sh
+++ docker-build.W2914NSv2.sh
@@ -2,5 +2,4 @@
 
 docker run --rm -it -v $PWD/bin:/home/build/openwrt/bin \
     -v $PWD/dl:/home/build/openwrt/dl \
-    -v $PWD/conf/config.w2914nsv2:/home/build/openwrt/.config \
-    openwrt-builder:latest
+    openwrt-builder:latest-w2914nsv2
