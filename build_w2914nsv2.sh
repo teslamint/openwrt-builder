07929f6b2eb097669effd8fb6550bbff8aada0f2
Jaehoon You <teslamint@gmail.com>
2020-02-01 21:46:23 +0900

    add W2914NSv2 build script
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

---
+++ build_w2914nsv2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MAKEOPTS=$*
+MAKEOPTS=${MAKEOPTS:-"-j7"}
+
+# clone openwrt and packages
+git clone --depth=1 https://github.com/openwrt/openwrt
+git clone --depth=1 https://github.com/stangri/openwrt_packages
+
+# copy some configs
+cp conf/config.w2914nsv2 openwrt/.config
+cp conf/feeds.conf openwrt/
+
+pushd openwrt
+
+# prepare package
+scripts/feeds update -a
+cp -Rfv ../openwrt_packages/vpn-policy-routing feeds/packages/net/
+cp -Rfv ../openwrt_packages/luci-app-vpn-policy-routing feeds/luci/applications/
+scripts/feeds update packages
+scripts/feeds update luci
+scripts/feeds install -a
+
+# build
+make oldconfig
+make $MAKEOPTS
+popd
