fc60d981a963e6ed28d06bbbccc8902a1ac81831
Jaehoon You <teslamint@gmail.com>
2020-02-02 09:22:57 +0900

    rename tag
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --rm -f Dockerfile.base -t 'openwrt-builder:latest-base' .
-docker build --rm -f Dockerfile.a604m -t 'openwrt-builder:latest-a604m' .
-docker build --rm -f Dockerfile.w2914nsv2 -t 'openwrt-builder:latest-w2914nsv2' .
+docker build --rm -f Dockerfile.base -t 'teslamint/openwrt-builder:latest-base' .
+docker build --rm -f Dockerfile.a604m -t 'teslamint/openwrt-builder:latest-a604m' .
+docker build --rm -f Dockerfile.w2914nsv2 -t 'teslamint/openwrt-builder:latest-w2914nsv2' .
