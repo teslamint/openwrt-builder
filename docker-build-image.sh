f3015a7b07b53ab322df5784c87f45f1321da58b
Jaehoon You <teslamint@gmail.com>
2020-04-02 23:57:18 +0900

    add wireguard
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --rm -f Dockerfile.base -t 'teslamint/openwrt-builder:latest-base' .
-docker build --rm -f Dockerfile.a604m -t 'teslamint/openwrt-builder:latest-a604m' .
-docker build --rm -f Dockerfile.w2914nsv2 -t 'teslamint/openwrt-builder:latest-w2914nsv2' .
+docker builder build --rm -f Dockerfile.base -t 'teslamint/openwrt-builder:latest-base' .
+docker builder build --rm -f Dockerfile.a604m -t 'teslamint/openwrt-builder:latest-a604m' .
+docker builder build --rm -f Dockerfile.w2914nsv2 -t 'teslamint/openwrt-builder:latest-w2914nsv2' .
